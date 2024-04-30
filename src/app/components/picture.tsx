763e8213cab1e7d6ec0a401d67c7ae4846e3fb42
xsukari <43860627+xsukari@users.noreply.github.com>
2024-04-30 11:43:26 +0200

    added profile picture component and placeholder picture

---
+++ src/app/components/picture.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import fs from "fs"
+
+function getPicture(): string {
+    if (fs.existsSync(process.cwd() + "/public/portrait.jpg")) {
+        return "/portrait.jpg"
+    } else {
+        return "/placeholder_portrait.jpg"
+    }
+}
+
+export default function picture() {
+    return (
+        <Image
+            src={getPicture()}
+            alt="Portrait"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+        />
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
+import Picture from "./components/picture"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <main className="flex min-h-screen">
+            <div className="w-1/3 min-h-screen bg-[#2b3639]">
+                <Picture />
+
+                <div className="h-1/5">
+
+                </div>
+            </div>
+            <div className="w-2/3 min-h-screen bg-[#ffffff]">
 
             </div>
         </main>
-    );
+    )
 }
