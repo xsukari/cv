1ed5f02209c2365fa869d11502b044bb973d4f85
xsukari <43860627+xsukari@users.noreply.github.com>
2024-04-30 13:04:54 +0200

    added name component

---
+++ src/app/components/name.tsx
@@ -0,0 +1,50 @@
+import { Sono } from "next/font/google"
+import fs from "fs"
+import fsAsync from "fs/promises"
+
+const sono = Sono({ subsets: ["latin"] })
+const sonoLight = Sono({ subsets: ["latin"], weight: "300" })
+const values = {
+    firstName: "firstName",
+    lastName: "lastName",
+    title: "title"
+}
+
+function getFile(): string {
+    if (fs.existsSync(process.cwd() + "/public/data.json")) {
+        return process.cwd() + "/public/data.json"
+    } else {
+        return process.cwd() + "/public/placeholder_data.json"
+    }
+}
+
+async function getText(value: string): string {
+    const file = await fsAsync.readFile(getFile(), "utf8")
+    const data = JSON.parse(file)
+
+    return data[value].toUpperCase()
+}
+
+export default function name() {
+    return (
+        <div className="h-[23.5%]">
+            <div className="p-[6%] pt-[8%]">
+                <div className="text-7xl p-2 tracking-[.025em]">
+                    <div className={sonoLight.className}>
+                        {getText(values.firstName)}
+                    </div>
+                </div>
+                <div className="text-7xl p-2 tracking-[.025em]">
+                    <div className={sonoLight.className}>
+                        {getText(values.lastName)}
+                    </div>
+                </div>
+                <div className="text-2xl p-2 tracking-wide">
+                    <div className={sono.className}>
+                        {getText(values.title)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import Picture from "./components/picture"
+import Name from "./components/name"
 
 export default function Home() {
     return (
         <main className="flex min-h-screen">
             <div className="w-1/3 min-h-screen bg-[#2b3639]">
-                <Picture />
+                {/* Left column */}
 
+                <Picture />
                 <div className="h-1/5">
 
                 </div>
             </div>
             <div className="w-2/3 min-h-screen bg-[#ffffff]">
+                {/* Right column */}
+
+                <Name />
 
             </div>
         </main>
