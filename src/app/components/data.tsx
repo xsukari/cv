9c608477e1c401b4ebafa81121a3e7691b1659a5
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-02 13:06:19 +0200

    added data component, which is used to get data from data.json

---
+++ src/app/components/data.tsx
@@ -0,0 +1,55 @@
+import fs from "fs"
+
+export const categories = {
+    person: "person",
+    contact: "contact",
+    education: "education",
+    languages: "languages",
+    skills: "skills",
+    experience: "experience"
+}
+
+export interface Person {
+    firstName: string,
+    lastName: string,
+    title: string
+}
+
+export interface Contact {
+    mobile: string,
+    mail: string,
+    address: string,
+    repo: string,
+    social: string,
+    homepage: string
+}
+
+export interface Education {
+    degree: string,
+    university: string,
+    location: string,
+    startDate: string,
+    endDate: string
+}
+
+function getDataFile(): string {
+    if (fs.existsSync(process.cwd() + "/public/data.json")) {
+        return process.cwd() + "/public/data.json"
+    } else {
+        return process.cwd() + "/public/placeholder_data.json"
+    }
+}
+
+export function getData(category: string): ( Person | Contact | Education[]) {
+    const file = fs.readFileSync(getDataFile(), "utf8")
+    const data = JSON.parse(file)
+
+    return data.elements[category]
+}
+
+export function getVisibilty(element: string): boolean {
+    const file = fs.readFileSync(getDataFile(), "utf8")
+    const data = JSON.parse(file)
+
+    return data.visibility[element]
+}
