19e1d2b978ffac65a52131eedd54ea8f8d1c1f15
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-02 13:04:46 +0200

    added new font

--- src/app/components/fonts.tsx
+++ src/app/components/fonts.tsx
@@ -1,4 +1,5 @@
-import { Sono } from "next/font/google"
+import { Sono, Open_Sans } from "next/font/google"
 
 export const sono = Sono({ subsets: ["latin"] })
 export const sonoLight = Sono({ subsets: ["latin"], weight: "300" })
+export const openSans = Open_Sans({ subsets: ["latin"] })
