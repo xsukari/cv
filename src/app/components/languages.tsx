3682317fba8d928dc27b0ee5ad5397d1acd882cd
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-02 13:42:46 +0200

    updated languages component

--- src/app/components/languages.tsx
+++ src/app/components/languages.tsx
@@ -1,11 +1,11 @@
-import { sonoLight, openSans } from "./fonts"
+import { sonoLight, sofiaSansSemiCondensed } from "./fonts"
 
 export default function languages() {
     return (
         <div className="h-1/4 text-white">
             <div className="text-2xl px-7 pt-5">
                 <div className={sonoLight.className}>
-                    Education
+                    Languages
                 </div>
 
             </div>
