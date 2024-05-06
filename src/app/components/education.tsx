a685c74d8b21707bb72db6fb56f3c9990d391dda
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-06 18:14:56 +0200

    limit maximum amount of used education entries

--- src/app/components/education.tsx
+++ src/app/components/education.tsx
@@ -3,31 +3,32 @@ import { sonoLight, sofiaSansSemiCondensed } from "./fonts"
 
 const _education = (data.getData(data.categories.education) as data.Education[])
     .sort((a, b) => parseInt(b.endYear) - parseInt(a.endYear))
+const educationLimit = _education.length > 2 ? 2 : _education.length
 
 function educationElements(): JSX.Element[] {
     const elements: JSX.Element[] = []
 
-    _education.forEach(element => {
+    for (let i = 0; i < educationLimit; i++) {
         elements.push (
             <div className="px-7 pt-5">
                 <div className="py-1">
                     <div className={sofiaSansSemiCondensed.className}>
-                        {element.degree}
+                        {_education[i].degree}
                     </div>
                 </div>
                 <div className="py-1">
                     <div className={sofiaSansSemiCondensed.className}>
-                        {element.university + ", " + element.location}
+                        {_education[i].university + ", " + _education[i].location}
                     </div>
                 </div>
                 <div className="py-1">
                     <div className={sofiaSansSemiCondensed.className}>
-                        {element.startYear + " - " + element.endYear}
+                        {_education[i].startYear + " - " + _education[i].endYear}
                     </div>
                 </div>
             </div>
         )
-    })
+    }
 
     return elements
 }
