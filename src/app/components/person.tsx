ccb998cef5276f79345352429b5f83bda97835c5
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-09 10:13:38 +0200

    unified indentation of main column (right)

--- src/app/components/person.tsx
+++ src/app/components/person.tsx
@@ -6,18 +6,18 @@ const _person = data.getData(data.categories.person) as data.Person
 export default function person() {
     return (
         <div className="h-[23.5%]">
-            <div className="p-[6%] pt-[8%]">
-                <div className="text-7xl p-2 tracking-[.025em]">
+            <div className="px-7 py-[8%]">
+                <div className="text-7xl py-2 tracking-[.025em]">
                     <div className={sonoLight.className}>
                         {_person.firstName.toUpperCase()}
                     </div>
                 </div>
-                <div className="text-7xl p-2 tracking-[.025em]">
+                <div className="text-7xl py-2 tracking-[.025em]">
                     <div className={sonoLight.className}>
                         {_person.lastName.toUpperCase()}
                     </div>
                 </div>
-                <div className="text-2xl p-2 tracking-wide">
+                <div className="text-2xl py-2 tracking-wide">
                     <div className={sono.className}>
                         {_person.title.toUpperCase()}
                     </div>
