4af5e1abda8150536f129dcd034a9f469d644665
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-15 13:57:07 +0200

    so apparently I wasn't using chromium, but a fork of it, called thorium, and it was quite outdated, so I had to adjust the size, in order for the page to properly print

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "./components/skills"
 
 export default function Home() {
     return (
-        <main className={"flex min-h-[1180px] min-w-[833px] max-h-[1180px] max-w-[833px] m-auto"}>
+        <main className={"flex min-h-[1178px] min-w-[833px] max-h-[1180px] max-w-[833px] m-auto"}>
             <div className="w-1/3 bg-[#2b3639]">
                 {/* Left column */}
 
